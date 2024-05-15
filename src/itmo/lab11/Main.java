93326d53cc95399da9a65424b11e0a19e43dbdbd
antonLoginov1985 <Owen21@yandex.ru>
2024-05-15 21:21:22 +0300

    исправление lab 11

--- src/itmo/lab11/Main.java
+++ src/itmo/lab11/Main.java
@@ -11,33 +11,18 @@ public class Main {
         //lessons 1,2
         for (int i = 0; i < 10; i++) {
             MyThread myThread = new MyThread();
-            System.out.println("Состояние потока "+ myThread.getName()+ " перед запуском "+ myThread.getState());
+            System.out.println("Состояние потока " + myThread.getName() + " перед запуском " + myThread.getState());
             myThread.start();
-            System.out.println("Состояние потока "+ myThread.getName()+ " после выполнения  "+ myThread.getState());
-         }
+            System.out.println("Состояние потока " + myThread.getName() + " после выполнения  " + myThread.getState());
+        }
 
-        //lessons 3 решение 1 через join
-        //Counter counter = new Counter();
-        //System.out.println(counter.getCount());
-        //for (int a = 1; a <= 1000; a++) {
-        //    MyThread2 myThread2 = new MyThread2(counter);
-
-        //    myThread2.start();
-//
-        //     try {
-        //         myThread2.join();
-        //     } catch (Exception e) {
-        //         System.out.println(e);
-        //     }
-        // }
-        //System.out.println(counter.getCount());
 
 
         //lessons 3 решение 2 через latch
         Counter counter = new Counter();
-        CountDownLatch latch = new CountDownLatch(1000);
+        CountDownLatch latch = new CountDownLatch(100);
         System.out.println(counter.getCount());
-        for (int a = 0; a < 1000; a++) {
+        for (int a = 0; a < 100; a++) {
             MyThread2 myThread2 = new MyThread2(counter, latch);
             myThread2.start();
         }
@@ -47,7 +32,7 @@ public class Main {
 
         //lessons 4
         Object lock = new Object();
-       MyThread3 myThread41 = new MyThread3(lock);
+        MyThread3 myThread41 = new MyThread3(lock);
         MyThread3 myThread42 = new MyThread3(lock);
         myThread41.start();
         myThread42.start();
